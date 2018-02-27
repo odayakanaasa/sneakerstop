bf6f19754158c8a666516f5647472d8f697125a0
JulianHinsch <jhinsch799@gmail.com>
2018-02-27 18:29:20 -0500

    removed auth-variables.js

--- client/src/utils/auth/auth-variables.js
+++
@@ -1,6 +0,0 @@
-export const AUTH_CONFIG = {
-    domain: 'sneaker-stop.auth0.com',
-    clientId: 'Re1Qk5gtuzJImf57FancOhHFf7U36ewv',
-    callbackUrl: 'http://localhost:3000/callback',
-    dbConnectionName: 'Username-Password-Authentication',
-}
